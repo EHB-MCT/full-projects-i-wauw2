fa2b25c61beac02a86207b9018e77b62ae65ac01
Yoran Asselman <yoran.asselman@student.ehb.be>
2022-01-31 15:38:11 +0100

    Portfolio: meerdere foto's klikken werkt nu

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,16 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var img = $('.myImg');
+var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-img.onclick = function () {
+$('.myImg').click(function () {
     modal.style.display = "block";
-    modalImg.src = this.src;
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
-}
+});
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
