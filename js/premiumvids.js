4558a7152a06fa16cf27a1e3a272239b2b55f5c9
Jelle Van Waeyenberghe <Jelle.van.waeyenberghe@student.ehb.be>
2022-02-01 15:25:35 +0100

    premium vids added, diy gepruts

---
+++ js/premiumvids.js
@@ -0,0 +1,16 @@
+function log() {
+    var naamEen = document.getElementById("premium").value;
+    var wachtwoord = document.getElementById("ww").value;
+    if (naamEen == "Test" && wachtwoord == "Test123" || naamEen == "Ilhan" && wachtwoord == "vrijdag") {
+        document.getElementById("vid1").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid1").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid2").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid2").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid3").href = "https://www.andleuven.com/en/video-3-staalslakmortel";
+        document.getElementById("vid3").innerHTML = "https://www.andleuven.com/en/video-3-staalslakmortel";
+        document.getElementById("vid4").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid4").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid5").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid5").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+    }
+}
