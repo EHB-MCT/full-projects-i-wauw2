9f64a5f9090b7f302cccc961527553f326e807ef
Jelle Van Waeyenberghe <Jelle.van.waeyenberghe@student.ehb.be>
2022-02-01 15:38:23 +0100

    links goed diy premium

--- js/premiumvids.js
+++ js/premiumvids.js
@@ -4,13 +4,13 @@ function log() {
     if (naamEen == "Test" && wachtwoord == "Test123" || naamEen == "Ilhan" && wachtwoord == "vrijdag") {
         document.getElementById("vid1").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
         document.getElementById("vid1").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
-        document.getElementById("vid2").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
-        document.getElementById("vid2").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid2").href = "https://www.andleuven.com/en/video-2-gerecycleerd-karton-papier";
+        document.getElementById("vid2").innerHTML = "https://www.andleuven.com/en/video-2-gerecycleerd-karton-papier";
         document.getElementById("vid3").href = "https://www.andleuven.com/en/video-3-staalslakmortel";
         document.getElementById("vid3").innerHTML = "https://www.andleuven.com/en/video-3-staalslakmortel";
-        document.getElementById("vid4").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
-        document.getElementById("vid4").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
-        document.getElementById("vid5").href = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
-        document.getElementById("vid5").innerHTML = "https://www.andleuven.com/en/video-1-wat-het-nieuwe-goud";
+        document.getElementById("vid4").href = "https://www.andleuven.com/en/video-4-recuperatie-plastics";
+        document.getElementById("vid4").innerHTML = "https://www.andleuven.com/en/video-4-recuperatie-plastics";
+        document.getElementById("vid5").href = "https://www.andleuven.com/en/video-5-algen-gietporselein";
+        document.getElementById("vid5").innerHTML = "https://www.andleuven.com/en/video-5-algen-gietporselein";
     }
 }
